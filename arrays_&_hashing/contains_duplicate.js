5a60bf49b171abcffd37865e19ed5185666ba616
Viet Tran <viettran101294@gmail.com>
2023-04-07 10:13:30 -0400

    Solved leetcode 217  Contains Duplicate

---
+++ arrays_&_hashing/contains_duplicate.js
@@ -0,0 +1,19 @@
+// 217. Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Input: nums = [1,2,3,1]
+// Output: true
+
+const containsDuplicate = function(nums) {
+  const placeholder = new Set();
+
+  for (const num of nums) {
+    if (placeholder.has(num)) {
+      return true;
+    } else {
+      placeholder.add(num);
+    }
+  }
+  return false;
+};
