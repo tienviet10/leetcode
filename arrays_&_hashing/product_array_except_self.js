2303c8058858fd60ac2fd8cb54bdb2ef9d33d0e6
Viet Tran <viettran101294@gmail.com>
2023-04-08 17:49:52 -0400

    Solved product array except self

---
+++ arrays_&_hashing/product_array_except_self.js
@@ -0,0 +1,29 @@
+// 238. Product of Array Except Self
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+const productExceptSelf = function(nums) {
+  const res = new Array(nums.length).fill(1);
+
+  let preProd = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    preProd *= nums[i];
+    res[i + 1] *= preProd;
+  }
+
+  let postProd = 1;
+  for (let i = nums.length - 1; i > 0; i--) {
+    postProd *= nums[i];
+    res[i - 1] *= postProd;
+  }
+
+  return res;
+};
