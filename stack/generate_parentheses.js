fe9654b29cb99dd9cdb800f7f0fb193221ac9054
Viet Tran <viettran101294@gmail.com>
2023-04-18 12:26:37 -0400

    Solved generate parentheses

---
+++ stack/generate_parentheses.js
@@ -0,0 +1,35 @@
+// 22. Generate Parentheses
+
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+const generateParenthesis = function(n) {
+  let stack = [],
+    res = [];
+
+  const runStack = (openP, closeP) => {
+    if (openP === closeP && closeP === n) {
+      res.push(stack.join(""));
+      return;
+    }
+
+    if (openP < n) {
+      stack.push("(");
+      runStack(openP + 1, closeP);
+      stack.pop();
+    }
+
+    if (closeP < openP) {
+      stack.push(")");
+      runStack(openP, closeP + 1);
+      stack.pop();
+    }
+  };
+
+  runStack(0, 0);
+  return res;
+};
