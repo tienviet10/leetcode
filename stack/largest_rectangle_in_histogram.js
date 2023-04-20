7a68a4b2d0b60a90257ca260a6b2a3de6f23f4f9
Viet Tran <viettran101294@gmail.com>
2023-04-20 12:06:10 -0400

    Solved Largest Rectangle in Histogram

---
+++ stack/largest_rectangle_in_histogram.js
@@ -0,0 +1,31 @@
+// 84. Largest Rectangle in Histogram
+
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+// Example 1:
+
+// Input: heights = [2,1,5,6,2,3]
+// Output: 10
+// Explanation: The above is a histogram where width of each bar is 1.
+// The largest rectangle is shown in the red area, which has an area = 10 units.
+
+const largestRectangleArea = function(heights) {
+  let stack = [],
+    maxArea = 0;
+
+  for (let i = 0; i < heights.length; i++) {
+    let start = i;
+    while (stack.length > 0 && stack[stack.length - 1][1] > heights[i]) {
+      const [index, height] = stack.pop();
+      maxArea = Math.max(maxArea, height * (i - index));
+      start = index;
+    }
+    stack.push([start, heights[i]]);
+  }
+
+  for (const [i, h] of stack) {
+    maxArea = Math.max(maxArea, h * (heights.length - i));
+  }
+
+  return maxArea;
+};
