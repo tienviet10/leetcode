19084ea4245c9c4fc44bbfeafc10ba9171f87f0c
Viet Tran <viettran101294@gmail.com>
2023-04-24 12:12:19 -0400

    Solved Longest Substring Without Repeating Characters

---
+++ sliding_window/longest_substring_without_repeating_characters.js
@@ -0,0 +1,27 @@
+// 3. Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest
+// substring without repeating characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+const lengthOfLongestSubstring = function(s) {
+  const substringHolder = new Set();
+  let l = 0,
+    res = 0;
+
+  for (let r = 0; r < s.length; r++) {
+    while (substringHolder.has(s[r])) {
+      substringHolder.delete(s[l]);
+      l++;
+    }
+    substringHolder.add(s[r]);
+    res = Math.max(res, r - l + 1);
+  }
+
+  return res;
+};
