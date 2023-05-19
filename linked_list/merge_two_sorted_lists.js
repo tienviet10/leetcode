aa5661450731ae60cca46c706b998550deac557c
Viet Tran <viettran101294@gmail.com>
2023-05-19 19:35:24 -0400

    Solved Merge two sorted lists

---
+++ linked_list/merge_two_sorted_lists.js
@@ -0,0 +1,25 @@
+const mergeTwoLists = function(list1, list2) {
+  let startNode = new ListNode(),
+    dummy = startNode;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      dummy.next = list1;
+      list1 = list1.next;
+    } else {
+      dummy.next = list2;
+      list2 = list2.next;
+    }
+    dummy = dummy.next;
+  }
+
+  if (list1) {
+    dummy.next = list1;
+  }
+
+  if (list2) {
+    dummy.next = list2;
+  }
+
+  return startNode.next;
+};
---
+++ linked_list/reverse_linked_list.js
@@ -0,0 +1,13 @@
+const reverseList = function(head) {
+  let prev = null,
+    curr = head;
+
+  while (curr) {
+    const temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+
+  return prev;
+};
