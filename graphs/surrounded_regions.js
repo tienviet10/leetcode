a9043c5d471f0b60e4fdca29abdabb0ce6f3230b
Viet Tran <viettran101294@gmail.com>
2023-06-01 02:47:29 -0400

    Solved Surrounded Regions

--- graphs/surrounded_regions.js
+++ graphs/surrounded_regions.js
@@ -14,6 +14,34 @@
 // The bottom 'O' is on the border, so it is not flipped.
 // The other three 'O' form a surrounded region, so they are flipped.
 
-const solve = function(board) {
 
+const solve = function(board) {
+  const dfs = (row, col) => {
+    if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] !== "O") return;
+    board[row][col] = "T";
+    dfs(row + 1, col);
+    dfs(row - 1, col);
+    dfs(row, col + 1);
+    dfs(row, col - 1);
+  };
+
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      if (board[row][col] === 'O' && (row === 0 || col === 0 || row === board.length - 1 || col === board[0].length - 1)) {
+        dfs(row, col);
+      }
+    }
+  }
+
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      if (board[row][col] === "T") {
+        board[row][col] = "O";
+      } else if (board[row][col] === "O") {
+        board[row][col] = "X";
+      }
+    }
+  }
+
+  return board;
 };
