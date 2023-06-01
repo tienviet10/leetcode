3fd1e7d84eac07761841ca6c35b66d8d1e413394
Viet Tran <viettran101294@gmail.com>
2023-06-01 02:20:38 -0400

    Solved Climbing Stairs

---
+++ dynamic_programming/climbing_stairs.js
@@ -0,0 +1,26 @@
+// * 70. Climbing Stairs
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+const climbStairs = function(n) {
+  if (n === 1) return 1;
+
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[dp.length - 1];
+};
