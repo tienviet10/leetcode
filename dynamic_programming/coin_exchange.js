5246d886b15c2d1a5182537354f715a61dba5d9f
Viet Tran <viettran101294@gmail.com>
2023-06-02 01:16:56 -0400

    Solved Coin Change

---
+++ dynamic_programming/coin_exchange.js
@@ -0,0 +1,29 @@
+// * 322. Coin Change
+
+// You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+// Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// You may assume that you have an infinite number of each kind of coin.
+
+// Example 1:
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+
+const coinChange = function(coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  coins.sort((a, b) => a - b);
+
+  for (let i = 1; i < dp.length; i++) {
+    for (let coin of coins) {
+      if (coin <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      } else {
+        break;
+      }
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
