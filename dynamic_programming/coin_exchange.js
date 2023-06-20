b6727f35cf5c4e2546945a783ad328dead002949
vtran-at-speer <viet.tran@speer.io>
2023-06-20 18:29:58 -0400

    Solved Cheapest Flights Within K Stops

--- dynamic_programming/coin_exchange.js
+++ dynamic_programming/coin_exchange.js
@@ -14,16 +14,14 @@
 const coinChange = function(coins, amount) {
   const dp = new Array(amount + 1).fill(Infinity);
   dp[0] = 0;
-  coins.sort((a, b) => a - b);
 
-  for (let i = 1; i < dp.length; i++) {
-    for (let coin of coins) {
-      if (coin <= i) {
+  for (let i = 1; i <= amount; i++) {
+    for (const coin of coins) {
+      if (i - coin >= 0) {
         dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-      } else {
-        break;
       }
     }
   }
+
   return dp[amount] === Infinity ? -1 : dp[amount];
 };
