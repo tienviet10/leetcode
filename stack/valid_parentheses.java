0a594c3e8ecaba9fe642ddb32dfb2dbb7746f082
vtran-at-speer <viet.tran@speer.io>
2023-06-22 01:40:48 -0400

    Solved some problem in java

---
+++ stack/valid_parentheses.java
@@ -0,0 +1,65 @@
+// * 20. Valid Parentheses
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+package stack;
+
+import java.util.Stack;
+
+class Solution {
+  public boolean isValid(String s) {
+    // ? Method 1:
+    // Stack<Character> stack = new Stack<>();
+
+    // for (char c : s.toCharArray()) {
+    // if (c == '(' || c == '[' || c == '{') {
+    // stack.push(c);
+    // } else {
+    // if (stack.isEmpty()) {
+    // return false;
+    // }
+
+    // char top = stack.pop();
+    // if ((c == ')' && top != '(') ||
+    // (c == ']' && top != '[') ||
+    // (c == '}' && top != '{')) {
+    // return false;
+    // }
+    // }
+    // }
+
+    // return stack.isEmpty();
+
+    // ? Method 2:
+    Stack<Character> stack = new Stack<>();
+
+    for (char c : s.toCharArray()) {
+      if (c == '(' || c == '[' || c == '{') {
+        stack.push(c);
+      } else {
+        if (stack.isEmpty()) {
+          return false;
+        }
+
+        char top = stack.pop();
+        if ((c == ')' && top != '(') ||
+            (c == ']' && top != '[') ||
+            (c == '}' && top != '{')) {
+          return false;
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+}
