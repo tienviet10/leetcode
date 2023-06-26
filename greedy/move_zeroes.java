98e4105f0c84d57b0e286716d3c5ce46eb7158f6
tienviet10 <viettran101294@gmail.com>
2023-06-26 19:17:05 -0400

    Solved Move Zeroes

---
+++ greedy/move_zeroes.java
@@ -0,0 +1,26 @@
+// * 283. Move Zeroes
+
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements. Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+//Input: nums = [0,1,0,3,12]
+//        Output: [1,3,12,0,0]
+
+package greedy;
+
+public class move_zeroes {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+        int r = 0;
+
+        while (r < nums.length) {
+            if (nums[r] != 0) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+            r++;
+        }
+    }
+}
